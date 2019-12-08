efba5f18761d5055548d5420f930aa11e201535a
kobishenkar <wkobiw@gmail.com>
2019-12-08 18:57:50 +0200

    changes 1

--- scripts.js
+++ scripts.js
@@ -2,6 +2,54 @@ function TicTacToeGame() {
 
     let table, cells, turn, new_cells;
 
+    const winCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    this.initGame = function () {
+        // get the table elemnt
+        // table = document.getElementById("table_game");
+        // console.log(table);
+
+        // get all cells
+        // cells = table.getElementsByTagName("td");
+        // console.log(cells);
+
+        turn = 0;
+        // ********refactoring********
+        new_cells = Array.from(document.querySelectorAll("td"))
+        console.log(new_cells);
+        for (let i = 0; i < new_cells.length; i++) {
+            new_cells[i].innerText = i;
+        }
+        initEvents();
+    }
+
+    function initEvents() {
+        for (let i = 0; i < new_cells.length; i++) {
+            new_cells[i].addEventListener("click", onCellClick)
+        }
+        let clearGameBtn = document.getElementById("button_clear");
+        clearGameBtn.addEventListener("click", clearGame);
+        let checkWinBtn = document.getElementById("button_check");
+        checkWinBtn.addEventListener("click", checkWin);
+    }
+
+    function onCellClick(event) {
+        var objCell = event.target;
+        if (turn) {
+            makeTurn(objCell, false)
+        } else {
+            makeTurn(objCell, true)
+        }
+    }
 
     function makeTurn(cell, isX) {
         if (cell.innerText === "X" || cell.innerText === "0") {
@@ -17,36 +65,13 @@ function TicTacToeGame() {
             turn = 0;
             cell.classList.add("ocupied");
         }
-
         cell.innerText = playerName;
-
-    }
-
-    function onCellClick(event) {
-        var objCell = event.target;
-        if (turn) {
-            makeTurn(objCell, false)
-        } else {
-            makeTurn(objCell, true)
-        }
-    }
-
-
-    function initEvents() {
-        for (let i = 0; i < cells.length; i++) {
-            const element = cells[i];
-            element.addEventListener("click", onCellClick)
-        }
-        let clearGameBtn = document.getElementById("button_clear");
-        clearGameBtn.addEventListener("click", clearGame);
-        let checkWinBtn = document.getElementById("button_check");
-        checkWinBtn.addEventListener("click", checkWin);
     }
 
     function clearGame() {
         console.clear();
-        for (let i = 0; i < cells.length; i++) {
-            const element = cells[i];
+        for (let i = 0; i < new_cells.length; i++) {
+            const element = new_cells[i];
             if (element.classList.contains("ocupied")) {
                 element.classList.remove("ocupied");
             }
@@ -58,120 +83,100 @@ function TicTacToeGame() {
     function checkWin() {
         var t0 = performance.now();
 
-        const cellsArr = [...table.lastChild.children]
-        let elementTrArr = [];
-        for (let i = 0; i < cellsArr.length; i++) {
-            let elementTr = [...cellsArr[i].children]
-            elementTrArr = [...elementTrArr, [...elementTr]];
-        }
+        // const cellsArr = [...table.lastChild.children]
+        // let elementTrArr = [];
+        // for (let i = 0; i < cellsArr.length; i++) {
+        //     let elementTr = [...cellsArr[i].children]
+        //     elementTrArr = [...elementTrArr, [...elementTr]];
+        // }
 
+        let horizontalArr = [[], [], []];
         let verticalArr = [[], [], []];
         let slantArr = [[], []];
-        for (let i = 0; i < elementTrArr.length; i++) {
-            let horizontalArr = [];
-            for (let j = 0; j < elementTrArr.length; j++) {
-                horizontalArr.push(elementTrArr[i][j].innerText);
+        for (let i = 0; i < new_cells.length; i++) {
 
-                if (i === j) { //slantArr_0
-                    slantArr[0].push(elementTrArr[i][j].innerText);
+            if (i < 3) {
+                horizontalArr[0].push(new_cells[i].innerText);
+            }
+            if (i >= 3 || i <= 5) {
+                horizontalArr[1].push(new_cells[i].innerText);
+            }
+            if (i > 5) {
+                horizontalArr[2].push(new_cells[i].innerText);
+            }
+
+            if (i % 4 === 0) { //slantArr_0
+                slantArr[0].push(new_cells[i].innerText);
+            }
+            if (i % 2 === 0 && i !== 0 && i !== 8) { //slantArr_1
+                slantArr[1].push(new_cells[i].innerText);
+            }
+            // start from here
+            if (i % 3 === 0) { // verticalArr_column_1 
+                verticalArr[0].push(new_cells[i].innerText);
+            } else if (j === 1) {  // verticalArr_column_2
+                verticalArr[1].push(elementTrArr[i][j].innerText);
+            } else if (j === 2) {  // verticalArr_column_3
+                verticalArr[2].push(elementTrArr[i][j].innerText);
+            }
+
+            if (j === 2 && elementTrArr[i][j].innerText !== "") {
+                if (horizontalArr.every((val, i, arr) => val === arr[0])) { // if horizontalArr have equals values -> win
+                    console.log("win in row: ", i + 1, horizontalArr);
+                    var t1 = performance.now();
+                    console.log("Call took " + (t1 - t0) + " milliseconds.");
+                    alert("win")
+                    return;
                 }
-                if (i + 2 === j || i & j === 1 || j + 2 === i) { //slantArr_1
-                    slantArr[1].push(elementTrArr[i][j].innerText);
+            }
+            if (i === 2) {
+                if (!verticalArr[0].includes("") && verticalArr[0].every((val, i, arr) => val === arr[0])) { // if verticalArr have equals values -> win
+                    console.log("win in column: ", j + 1, verticalArr[0]);
+                    var t1 = performance.now();
+                    console.log("Call took " + (t1 - t0) + " milliseconds.");
+                    alert("win")
+                    return;
                 }
-
-                if (j === 0) { // verticalArr_column_1 
-                    verticalArr[0].push(elementTrArr[i][j].innerText);
-                } else if (j === 1) {  // verticalArr_column_2
-                    verticalArr[1].push(elementTrArr[i][j].innerText);
-                } else if (j === 2) {  // verticalArr_column_3
-                    verticalArr[2].push(elementTrArr[i][j].innerText);
+                if (!slantArr[1].includes("") && slantArr[1].every((val, i, arr) => val === arr[0])) {
+                    console.log("win in slant right: ", slantArr[1]);
+                    var t1 = performance.now();
+                    console.log("Call took " + (t1 - t0) + " milliseconds.");
+                    alert("win")
+                    return;
                 }
-
-                if (j === 2 && elementTrArr[i][j].innerText !== "") {
-                    if (horizontalArr.every((val, i, arr) => val === arr[0])) { // if horizontalArr have equals values -> win
-                        console.log("win in row: ", i + 1, horizontalArr);
-                        var t1 = performance.now();
-                        console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.");
-                        alert("win")
-                        return;
-                    }
+            }
+            if (i === 2 && j === 1 && !verticalArr[1].includes("")) {
+                if (verticalArr[j].every((val, i, arr) => val === arr[0])) { // if verticalArr have equals values -> win
+                    console.log("win in column: ", j + 1, verticalArr[j]);
+                    var t1 = performance.now();
+                    console.log("Call took " + (t1 - t0) + " milliseconds.");
+                    alert("win")
+                    return;
                 }
-                if (i === 2) {
-                    if (!verticalArr[0].includes("") && verticalArr[0].every((val, i, arr) => val === arr[0])) { // if verticalArr have equals values -> win
-                        console.log("win in column: ", j + 1, verticalArr[0]);
-                        var t1 = performance.now();
-                        console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.");
-                        alert("win")
-                        return;
-                    }
-                    if (!slantArr[1].includes("") && slantArr[1].every((val, i, arr) => val === arr[0])) {
-                        console.log("win in slant right: ", slantArr[1]);
-                        var t1 = performance.now();
-                        console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.");
-                        alert("win")
-                        return;
-                    }
-                }
-                if (i === 2 && j === 1 && !verticalArr[1].includes("")) {
-                    if (verticalArr[j].every((val, i, arr) => val === arr[0])) { // if verticalArr have equals values -> win
-                        console.log("win in column: ", j + 1, verticalArr[j]);
-                        var t1 = performance.now();
-                        console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.");
-                        alert("win")
-                        return;
-                    }
-                }
-                if (i === 2 && j === 2) {
-                    if (!verticalArr[2].includes("") && verticalArr[j].every((val, i, arr) => val === arr[0])) { // if verticalArr have equals values -> win
-                        console.log("win in column: ", j + 1, verticalArr[j]);
-                        var t1 = performance.now();
-                        console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.");
-                        alert("win")
-                        return;
-                    }
-
-                    if (!slantArr[0].includes("") && slantArr[0].every((val, i, arr) => val === arr[0])) { // if slantArr have equals values -> win
-                        console.log("win in slant left: ", slantArr[0]);
-                        var t1 = performance.now();
-                        console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.");
-                        alert("win")
-                        return;
-                    }
-
-
-
+            }
+            if (i === 2 && j === 2) {
+                if (!verticalArr[2].includes("") && verticalArr[j].every((val, i, arr) => val === arr[0])) { // if verticalArr have equals values -> win
+                    console.log("win in column: ", j + 1, verticalArr[j]);
+                    var t1 = performance.now();
+                    console.log("Call took " + (t1 - t0) + " milliseconds.");
+                    alert("win")
+                    return;
                 }
 
+                if (!slantArr[0].includes("") && slantArr[0].every((val, i, arr) => val === arr[0])) { // if slantArr have equals values -> win
+                    console.log("win in slant left: ", slantArr[0]);
+                    var t1 = performance.now();
+                    console.log("Call took " + (t1 - t0) + " milliseconds.");
+                    alert("win")
+                    return;
+                }
             }
         }
-        console.log(verticalArr);
+        // console.log(verticalArr);
         var t1 = performance.now();
-        console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.");
+        console.log("Call took " + (t1 - t0) + " milliseconds.");
     }
-
-
-    this.initGame = function (tableID) {
-        // get the table elemnt
-        table = document.getElementById("table_game");
-        console.log(table);
-
-        // get all cells
-        cells = table.getElementsByTagName("td");
-        console.log(cells);
-
-        turn = 0;
-        initEvents();
-
-
-        // ********refactoring********
-        new_cells = Array.from(document.querySelectorAll("td"))
-        console.log(new_cells);
-
-    }
-
 }
 
 let objGame = new TicTacToeGame();
-objGame.initGame("table_game");
-
-
+objGame.initGame();
